6277b5fef35db11008ceb3b378ac869c90dfa644
D3D53C <marc.steinebrunner@gmail.com>
2017-08-19 17:00:47 +0200

    Create main02.py

---
+++ main02.py
@@ -0,0 +1,14 @@
+def main:
+  import time
+  from LED import LED_C as LED_NO
+  Motor = LED_NO(11)
+  
+  while True:
+    Motor.ON()
+    time.sleep(2)
+    Motor.OFF()
+    time.sleep(2)
+    
+  
+if __name__ == '__main__':
+main()
