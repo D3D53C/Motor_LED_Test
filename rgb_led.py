29bec0ad506bcf0f44c50e228b6ef1cee742ff8a
D3D53C <marc.steinebrunner@gmail.com>
2017-08-20 21:47:49 +0200

    Add files via upload

---
+++ rgb_led.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""rgb_led.py:	RGB_LED class"""
+
+__author__ = "Marc Steinebrunner"
+__copyright__ = "Copyright (c) 2017 Marc Steinebrunner"
+__version__ = "Development v0.01"
+__email__ = "marc.steinebrunner@gmail.com"
+__status__ = "Dev"
+
+import RPi.GPIO as GPIO
+
+class RGB_LED(object):
+
+    def __init__(self, Pin_R, Pin_G, Pin_B):
+        self.Pin = [Pin_R, Pin_G, Pin_B]
+        GPIO.setmode(GPIO.BOARD)
+
+        for i in (self.Pin[0],self.Pin[1],self.Pin[2]):
+            GPIO.setup(i, GPIO.OUT)
+
+        self.frequenz
+        
+        self.PWM = [GPIO.PWM(self.Pin[0], self.frequenz), GPIO.PWM(self.Pin[1], self.frequenz), GPIO.PWM(self.Pin[2], self.frequenz)]
+        self.RGB = [0, 0, 0]
+        self.status = False
+
+
+    def hex_to_rgb(self, hex):
+        self.RGB[0] = int(hex[0:0 + 2], 16)
+        self.RGB[1] = int(hex[2:2 + 2], 16)
+        self.RGB[2] = int(hex[4:4 + 2], 16)
+
+    def on(self, hex):
+        self.hex_to_rgb(hex)
+        self.PWM[0].ChangeDutyCycle(self.RGB[0])
+        self.PWM[1].ChangeDutyCycle(self.RGB[1])
+        self.PWM[2].ChangeDutyCycle(self.RGB[2])
+
+    def off(self):
+        for i in range(0,2):
+            self.PWM[i].stop()
+
+    def __del__(self):
+        for i in range(0 ,2):
+            GPIO.cleanup(self.Pin)
