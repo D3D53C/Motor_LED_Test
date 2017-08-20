db3b5e8c65b45b6712596b09ca540d5005c66085
D3D53C <marc.steinebrunner@gmail.com>
2017-08-20 13:56:11 +0200

    Update RGB_HEX.py

--- RGB_HEX.py
+++ RGB_HEX.py
@@ -5,4 +5,7 @@ print("R = {}".format(int(h[0:0+2], 16)))
 print("G = {}".format(int(h[2:2+2], 16)))
 print("B = {}".format(int(h[4:4+2], 16)))
 
-print("RGB => {}, {}, {}".format(int(h[i:i+2], 16) for i in (0, 2 ,4))))
+s1 = set([])
+for i in range 3:
+  si.add(int(h[i:i+2], 16) for i in (0, 2 ,4))))
+print("RGB => {}, {}, {}".format()
