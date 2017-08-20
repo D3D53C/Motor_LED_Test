74ed8d1d16e2495050fd5489ee483bf8e37b4663
D3D53C <marc.steinebrunner@gmail.com>
2017-08-20 13:48:49 +0200

    Update RGB_HEX.py

--- RGB_HEX.py
+++ RGB_HEX.py
@@ -1,5 +1,8 @@
 h = input('Enter hex: ').lstrip('#')
 print('RGB =', tuple(int(h[i:i+2], 16) for i in (0, 2 ,4)))
+
 print("R = {}".format(int(h[0:0+2], 16)))
 print("G = {}".format(int(h[2:2+2], 16)))
 print("B = {}".format(int(h[4:4+2], 16)))
+
+print("RGB => {}, {}, {}".format(int(h[i:i+2], 16) for i in (0, 2 ,4))))
