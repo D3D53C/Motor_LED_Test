fa5d8208a92b7766f8842b76c8045ed48c32018b
D3D53C <marc.steinebrunner@gmail.com>
2017-08-20 14:30:03 +0200

    Update RGB_HEX.py

--- RGB_HEX.py
+++ RGB_HEX.py
@@ -7,7 +7,7 @@ print("R = {}".format(int(h[0:0+2], 16)))
 print("G = {}".format(int(h[2:2+2], 16)))
 print("B = {}".format(int(h[4:4+2], 16)))
 
-for i in range(0, 2)
+for i in range(0, 2):
   print("RGB {}".format(int(h[(i*2):(i*2)+2], 16))
 
 #s1 = []
