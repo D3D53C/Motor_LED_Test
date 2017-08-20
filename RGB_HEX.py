b2d2b280a4e9ff2a6c6d92a30e1ff94cd9747607
D3D53C <marc.steinebrunner@gmail.com>
2017-08-20 13:44:36 +0200

    Update RGB_HEX.py

--- RGB_HEX.py
+++ RGB_HEX.py
@@ -1,20 +1,5 @@
-def hex_to_rgb(value):
-    value = value.lstrip('#')
-    lv = len(value)
-    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
-
-
-def rgb_to_hex(rgb):
-    return '#%02x%02x%02x' % rgb
-
-hex_to_rgb("#ffffff")              # ==> (255, 255, 255)
-hex_to_rgb("#ffffffffffff")        # ==> (65535, 65535, 65535)
-rgb_to_hex((255, 255, 255))        # ==> '#ffffff'
-rgb_to_hex((65535, 65535, 65535))  # ==> '#ffffffffffff'
-
-print('Please enter your colour hex')
-
-hex == input("")
-
-print('Calculating...')
-print(hex_to_rgb(hex()))
+h = input('Enter hex: ').lstrip('#')
+print('RGB =', tuple(int(h[i:i+2], 16) for i in (0, 2 ,4)))
+print("R = {}".format(int(h[0:0+2], 16)))
+print("G = {}".format(int(h[2:2+2], 16)))
+print("B = {}".format(int(h[4:4+2], 16)))
