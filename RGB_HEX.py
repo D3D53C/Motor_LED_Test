6043552857f6cd6e3a15152886a3b6994270f7ee
D3D53C <marc.steinebrunner@gmail.com>
2017-08-20 14:44:19 +0200

    Update RGB_HEX.py

--- RGB_HEX.py
+++ RGB_HEX.py
@@ -6,4 +6,6 @@ s1[0] = int(h[0:0+2], 16)
 s1[1] = int(h[2:2+2], 16)
 s1[2] = int(h[4:4+2], 16)
 
-print("RGB {0:f}, {1:f}, {2:f}".format(s1[0],s1[1],s1[2]))
+print(s1[0])
+print(s1[1])
+print(s1[2])
